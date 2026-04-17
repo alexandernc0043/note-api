aa11bcf8b5acdb2b9c5b4238f22fcd6640d2a3d6
Alexander Prechtel <aprechtel0043@gmail.com>
2026-04-16 20:05:19 -0400

    modify test case

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,21 +1,5 @@
 import request from 'supertest';
 import app from '../src/app.js';
-describe('Get all Notes', () => {
-  it('GET /notes should return an array of notes', async () => {
-    const res = await request(app).get('/notes');
-    expect(res.statusCode).toBe(200);
-    expect(res.body).toBeInstanceOf(Array);
-    expect(res.body).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(Number),
-          title: expect.any(String),
-          content: expect.any(String),
-        }),
-      ]),
-    );
-  });
-});
 describe('Create a Notes', () => {
   it('POST /notes should return a note', async () => {
     const res = await request(app)
@@ -30,6 +14,22 @@ describe('Create a Notes', () => {
         title: expect.any(String),
         content: expect.any(String),
       }),
+    );
+  });
+});
+describe('Get all Notes', () => {
+  it('GET /notes should return an array of notes', async () => {
+    const res = await request(app).get('/notes');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toBeInstanceOf(Array);
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          title: expect.any(String),
+          content: expect.any(String),
+        }),
+      ]),
     );
   });
 });
