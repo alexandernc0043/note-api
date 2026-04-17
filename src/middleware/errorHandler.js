af289d71dbde62e3576eff597d5c437864b14ecf
Alexander Prechtel <aprechtel0043@gmail.com>
2026-04-16 20:00:05 -0400

    attempting to fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,6 @@
 function errorHandler(err, req, res, next) {
   const status = err.status || 500;
+  console.log(err.message);
   res.status(status).json({
     error: err.message || 'Internal Server Error',
   });
